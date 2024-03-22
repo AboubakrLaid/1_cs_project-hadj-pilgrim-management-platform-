38c6f2dd3f791fd830f5b4333114fcfa232fc2cf
AboubakrLaid <belmiloudaboubakrdz@gmail.com>
2024-03-22 02:57:34 +0100

    add endpoint for participants statistics

--- server/api/lottery/urls.py
+++ server/api/lottery/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from .views import (
     participate_in_lottery,
-    lottery_algorithm
+    lottery_algorithm,
+    statistics,
     
 )
 
 urlpatterns = [
     path('', participate_in_lottery),
     path('algorithm', lottery_algorithm),
+    path('statistics', statistics),
+    # path('v2/statistics', optimized_statistics),
 ]
