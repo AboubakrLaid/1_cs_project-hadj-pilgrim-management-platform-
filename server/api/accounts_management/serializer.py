3764fbc06302c866cc227f1057cb6a985673d45f
ChaibFakhreddine <133677862+ChaibFakhreddine@users.noreply.github.com>
2024-03-27 12:36:57 +0100

    Update serializer.py

--- server/api/accounts_management/serializer.py
+++ server/api/accounts_management/serializer.py
@@ -1,1 +1,8 @@
+from rest_framework import serializers
+from .models import MedicalAdminProfile
+
+class MedicalAdminProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MedicalAdminProfile
+        fields = '__all__'
 
