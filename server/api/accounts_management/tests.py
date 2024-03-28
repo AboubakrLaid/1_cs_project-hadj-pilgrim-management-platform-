b31dd1fcb313f8f9ec8be5920ea9993abb77b45c
asma-benzaoucha <a.benzaoucha@esi-sba.dz>
2024-03-28 14:08:18 +0100

    add medial admin

--- server/api/accounts_management/tests.py
+++ server/api/accounts_management/tests.py
@@ -1,60 +1,32 @@
 from django.test import TestCase
 from users.models import User
 from municipal_wilaya.models import Wilaya, Hospital
-from .models import MedicalAdminProfile, AdminProfile
-from django.contrib.contenttypes.models import ContentType
+from .serializers import MedicalAdminProfileSerializer
 
+from rest_framework.test import APIRequestFactory
 
-# Create your tests here.
+class MedicalAdminProfileSerializerTestCase(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.request = self.factory.post('/api/add_medical_admin/', {
+            "user": {
+                "first_name": "John",
+                "last_name": "Doe",
+                "email": "john.doe@example.com",
+                "password": "password123",
+                "gender": "M"
+            },
+            "hospital_id": 1,
+            "work_schedule": {
+                "monday": "9-5",
+                "tuesday": "9-5",
+                "wednesday": "9-5",
+                "thursday": "9-5",
+                "friday": "9-5"
+            }
+        })
+        self.serializer = MedicalAdminProfileSerializer(context={'request': self.request})
 
-
-class ResponisbilityTestCase(TestCase):
-        def setUp(self):
-            self.admin = User.objects.create_user(
-                username="admin",
-                first_name="admin",
-                last_name="admin",
-                password="admin",
-                email="admin@gamil.com",
-                gender="M",
-                role=User.IS_ADMIN,
-            )
-            self.medical_admin = User.objects.create_user(
-                username="madmin",
-                first_name="madmin",
-                last_name="madmin",
-                password="madmin",
-                email="madmin@gamil.com",
-                gender="M",
-                role=User.IS_MEDICAL_ADMIN,
-            )
-            self.wilaya = Wilaya.objects.create(name='Mostaganem', eng_name='Mostaganem')
-            self.hospital = Hospital.objects.create(name='Hospital', wilaya_id=self.wilaya, eng_name='Mostaganem')
-            
-            
-            self.medical_admin_profile = MedicalAdminProfile.objects.create(
-                user=self.medical_admin,
-                content_object=self.hospital,
-                object_id=self.hospital.id,
-                work_schedule={'Monday':['08:00']}
-            )
-            
-            self.admin_profile = AdminProfile.objects.create( user=self.admin,
-                content_object=self.wilaya,
-                object_id=self.wilaya.id
-            )
-        
-        def test_medical_admin_profile(self, ):
-            self.assertEqual(self.medical_admin_profile.user, self.medical_admin)
-            self.assertEqual(self.medical_admin_profile.content_object, self.hospital)
-            self.assertEqual(self.medical_admin_profile.work_schedule, {'Monday':['08:00']})
-            self.assertNotEqual(self.medical_admin_profile.content_type, ContentType.objects.get_for_model(Wilaya))
-            self.assertEqual(self.medical_admin_profile.content_type, ContentType.objects.get_for_model(Hospital))
-            
-        def test_admin_profile(self):
-            self.assertEqual(self.admin_profile.user, self.admin)
-            self.assertEqual(self.admin_profile.content_object, self.wilaya)
-            self.assertNotEqual(self.admin_profile.content_type, ContentType.objects.get_for_model(Hospital))
-            self.assertEqual(self.admin_profile.content_type, ContentType.objects.get_for_model(Wilaya))
-
-    
+    def test_create_medical_admin_profile(self):
+        self.assertTrue(self.serializer.is_valid())
+        medical_admin_profile = self.serializer.save()
