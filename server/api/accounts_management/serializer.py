bba4a16019c6d9be9ddf2f818a09bf7f0535de8d
ChaibFakhreddine <133677862+ChaibFakhreddine@users.noreply.github.com>
2024-03-28 22:43:49 +0100

    Update serializer.py

--- server/api/accounts_management/serializer.py
+++ server/api/accounts_management/serializer.py
@@ -6,3 +6,33 @@ class MedicalAdminProfileSerializer(serializers.ModelSerializer):
         model = MedicalAdminProfile
         fields = '__all__'
 
+
+# condidat 
+from .models import User, AdminProfile, PersonalInfo, CompanionInfo, UserInscriptionHistory
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'first_name', 'last_name', 'nin', 'phone_number', 'role', 'wilaya', 'municipal', 'gender', 'inscription_year']
+
+class PersonalInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PersonalInfo
+        fields = '__all__'
+
+class CompanionInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CompanionInfo
+        fields = '__all__'
+
+class UserInscriptionHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserInscriptionHistory
+        fields = '__all__'
+
+
+
+class AdminProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AdminProfile
+        fields = '__all__'
