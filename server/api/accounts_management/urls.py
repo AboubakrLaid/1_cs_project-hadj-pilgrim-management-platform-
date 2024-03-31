89f831b244005e67f7c2a1ec26e88592d0dbc120
ChaibFakhreddine <133677862+ChaibFakhreddine@users.noreply.github.com>
2024-03-31 22:53:27 +0100

    Update urls.py

--- server/api/accounts_management/urls.py
+++ server/api/accounts_management/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('search-medical-admins/', views.search_medical_admins, name='search_medical_admins'),
+    # new
+    path('add_guide/', views.add_guide, name='add_guide'),
+    path('delete_guide/<int:guide_id>/', views.delete_guide, name='delete_guide'),
+    path('edit_candidate_info/<int:candidate_id>/', views.edit_candidate_info, name='edit_candidate_info'),
+    path('search_users/', views.search_users, name='search_users'),
     
 ]
