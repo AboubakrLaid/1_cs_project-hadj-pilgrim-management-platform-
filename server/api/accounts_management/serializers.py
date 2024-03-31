512a4c2340885854f3275695a6c7d599098fd452
ChaibFakhreddine <133677862+ChaibFakhreddine@users.noreply.github.com>
2024-03-31 22:55:09 +0100

    Update serializers.py

--- server/api/accounts_management/serializers.py
+++ server/api/accounts_management/serializers.py
@@ -1,1 +1,48 @@
+from rest_framework import serializers
+from .models import MedicalAdminProfile, Candidate, Guide, MedicalAdmin, User, AdminProfile, PersonalInfo, CompanionInfo, UserInscriptionHistory
+
+class MedicalAdminProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MedicalAdminProfile
+        fields = '__all__'
+
+class CandidateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Candidate
+        fields = '__all__'
+
+class GuideSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Guide
+        fields = '__all__'
+
+class MedicalAdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MedicalAdmin
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'first_name', 'last_name', 'nin', 'phone_number', 'role', 'wilaya', 'municipal', 'gender', 'inscription_year']
+
+class PersonalInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PersonalInfo
+        fields = '__all__'
+
+class CompanionInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CompanionInfo
+        fields = '__all__'
+
+class UserInscriptionHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserInscriptionHistory
+        fields = '__all__'
+
+class AdminProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AdminProfile
+        fields = '__all__'
 
