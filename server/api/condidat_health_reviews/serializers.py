772fbb9b04154ab2323d0b5b9b777058d8ba50d4
asma-benzaoucha <a.benzaoucha@esi-sba.dz>
2024-05-11 13:42:49 +0100

    sprint3 health review

--- server/api/condidat_health_reviews/serializers.py
+++ server/api/condidat_health_reviews/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import PatientHealthReview
-
+from users.models import UserStatus
 
 class PatientHealthReviewSerializer(serializers.ModelSerializer):
     treatments = serializers.ListField(child=serializers.CharField(max_length=255), allow_empty=True)
@@ -14,45 +14,45 @@ class PatientHealthReviewSerializer(serializers.ModelSerializer):
         if data.get('is_sick') and not data.get('is_healthy'):
             if not data.get('diseases'):
                 raise serializers.ValidationError({'diseases': 'Diseases are required for a sick patient.'})
-        
         return data
+
+    def update_user_status(self, instance, validated_data):
+        user = instance.user
+        user_status = user.status
+        if validated_data.get('is_accepted') is True:
+            user_status.status = UserStatus.Status.CONFIRMED
+            user_status.process = UserStatus.Process.PAYMENT
+        elif validated_data.get('is_accepted') is False:
+            user_status.status = UserStatus.Status.REJECTED
+        user_status.save()
+
+    def update(self, instance, validated_data):
+        self.update_user_status(instance, validated_data)
+        treatments = validated_data.pop('treatments', [])
+        diseases = validated_data.pop('diseases', [])
+        instance = super().update(instance, validated_data)
+        instance.treatments = treatments
+        instance.diseases = diseases
+        instance.save()
+        return instance
 
     def create(self, validated_data):
         if validated_data.get('is_sick') and not validated_data.get('is_healthy'):
-            treatments = validated_data.pop('treatments', [])
-            diseases = validated_data.pop('diseases', [])
             instance = super().create(validated_data)
-            instance.treatments = treatments
-            instance.diseases = diseases
-            instance.save()
-            return instance
-        else:
-            raise serializers.ValidationError({'is_sick': 'Patient must be sick to add diseases or treatments.'})
-
-
-# from users.models import UserStatus
-# from users.models import UserStatus.Status, UserStatus.Process
-
-
-
-    def update(self, instance, validated_data):
-        # Handle the logic when is_accepted is set to True
-        if validated_data.get('is_accepted') is True:
-            instance.is_accepted = True
-            instance.process = 'payment'
-            instance.status = 'pending'
-        # Handle the logic when is_accepted is set to False
-        elif validated_data.get('is_accepted') is False:
-            instance.status = 'refused'
-            # WE keep the process as it is
-        # Update other fields as needed
-        if validated_data.get('is_sick') and not validated_data.get('is_healthy'):
             treatments = validated_data.pop('treatments', [])
             diseases = validated_data.pop('diseases', [])
-            instance = super().update(instance, validated_data)
             instance.treatments = treatments
             instance.diseases = diseases
             instance.save()
             return instance
         else:
             raise serializers.ValidationError({'is_sick': 'Patient must be sick to add diseases or treatments.'})
+
+
+
+
+
+
+
+
+
