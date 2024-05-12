4a71f18caec8865f27d4c740a9ac57ee52b0127f
Lalliche <123468913+Lalliche@users.noreply.github.com>
2024-05-12 23:21:55 +0100

    Update PatientHealthReview.jsx

--- frontend/src/content/Doctor/PatientHealthReview.jsx
+++ frontend/src/content/Doctor/PatientHealthReview.jsx
@@ -1,28 +1,11 @@
-import { useState } from "react";
-import {
-  Box,
-  Typography,
-  Checkbox,
-  TextField,
-  Button,
-  FormControlLabel,
-  FormGroup,
-  IconButton,
-} from "@mui/material";
+import React, { useState } from "react";
+import { Box, Typography, Checkbox, TextField, Button, FormControlLabel, FormGroup, IconButton } from "@mui/material";
 import { Add, Remove, CloudUpload } from "@mui/icons-material";
 import FileDownloadIcon from "@mui/icons-material/FileDownload";
 import BloodtypeIcon from "@mui/icons-material/Bloodtype";
 import CloseIcon from "@mui/icons-material/Close";
-import PropTypes from "prop-types";
 
-function PatientHealthReview({ onClose, user }) {
-  PatientHealthReview.propTypes = {
-    onClose: PropTypes.func.isRequired,
-    user: PropTypes.object.isRequired,
-  };
-
-  console.log(user);
-
+function PatientHealthReview() {
   const [isSick, setIsSick] = useState(false);
   const [diseases, setDiseases] = useState([{ name: "", treatments: [""] }]);
   const [bloodType, setBloodType] = useState("");
@@ -74,8 +57,7 @@ function PatientHealthReview({ onClose, user }) {
 
   const handleTreatmentChange = (diseaseIndex, treatmentIndex, event) => {
     const updatedDiseases = [...diseases];
-    updatedDiseases[diseaseIndex].treatments[treatmentIndex] =
-      event.target.value;
+    updatedDiseases[diseaseIndex].treatments[treatmentIndex] = event.target.value;
     setDiseases(updatedDiseases);
   };
 
@@ -91,10 +73,6 @@ function PatientHealthReview({ onClose, user }) {
     setFile(event.target.files[0]);
   };
 
-  const handleDownloadPDF = (user) => {
-    // Handle PDF download
-  };
-
   const handleAccept = () => {
     // Handle accept action
   };
@@ -108,250 +86,132 @@ function PatientHealthReview({ onClose, user }) {
   };
 
   return (
-    <div
-      className="auth-body"
-      style={{
+    <div className="auth-body" style={{ display: "flex", justifyContent: "center", alignItems: "center" }} >
+      <Box sx={{
+        position: "relative",
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-      }}
-    >
-      <Box
-        sx={{
-          position: "relative",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          maxWidth: "634px",
-          maxHeight: "750px",
-          backgroundColor: "#ffffff",
-          borderRadius: "20px",
-          padding: "20px",
-          margin: "auto",
-          mt: "50px",
-          marginTop: "10px",
-        }}
-      >
-        <Typography
-          variant="h5"
-          align="center"
-          gutterBottom
-          sx={{
-            fontFamily: "Poppins",
-            maxFontSize: "30px",
-            fontWeight: 700,
-            lineHeight: "22px",
-            textAlign: "center",
-          }}
-        >
+        maxWidth: "800px",
+        maxHeight: "750px",
+        backgroundColor: "#ffffff",
+        borderRadius: "20px",
+        padding: "20px",
+        margin: "auto",
+        mt: "50px",
+        marginTop: "10px",
+        boxShadow: `0px 0px 5px rgba(0, 0, 0, 0.2)` /* Apply shadow with border color */,
+      }}>
+        <Typography variant="h5" align="center" gutterBottom sx={{
+          fontFamily: "Poppins",
+          maxFontSize: "30px",
+          fontWeight: 700,
+          lineHeight: "22px",
+          textAlign: "center",
+        }}>
           Patient Health Review
         </Typography>
-        <IconButton
-          onClick={handleExit}
-          sx={{
-            position: "absolute",
-            top: "5px",
-            right: "5px",
-            color: "rgb(0, 0, 0, 0.7)",
-          }}
-        >
-          <CloseIcon onClick={onClose} />
+        <IconButton onClick={handleExit} sx={{ position: "absolute", top: "5px", right: "5px", color: "rgb(0, 0, 0, 0.7)" }}>
+          <CloseIcon />
         </IconButton>
         <FormGroup>
           <FormControlLabel
-            control={
-              <Checkbox
-                checked={!isSick}
-                onChange={handleCheck}
-                sx={{ color: "#AB7595" }}
-              />
-            }
+            control={<Checkbox checked={!isSick} onChange={handleCheck} sx={{ color: "#AB7595" }} />}
             label="Healthy"
           />
           <FormControlLabel
-            control={
-              <Checkbox
-                checked={isSick}
-                onChange={handleCheck}
-                sx={{ color: "#AB7595" }}
-              />
-            }
+            control={<Checkbox checked={isSick} onChange={handleCheck} sx={{ color: "#AB7595" }} />}
             label="Sick"
           />
           {isSick && (
             <Box sx={{ width: "100%" }}>
               {diseases.map((disease, index) => (
-                <Box
-                  key={index}
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    width: "100%",
-                    marginBottom: "10px",
-                  }}
-                >
-                  <IconButton
-                    onClick={() => handleRemoveDisease(index)}
-                    sx={{ color: "#D01717" }}
-                  >
+                <Box key={index} sx={{ display: "flex", alignItems: "center", width: "100%", marginBottom: "10px" }}>
+                  <IconButton onClick={() => handleRemoveDisease(index)} sx={{ color: "red" }}>
                     <Remove />
                   </IconButton>
                   <TextField
                     placeholder={`Disease ${index + 1}`}
                     value={disease.name}
                     onChange={(e) => handleDiseaseNameChange(index, e)}
-                    sx={{
-                      width: "calc(45% - 30px)",
-                      marginRight: "5px",
-                      borderColor: isSick ? "#AB7595" : undefined,
-                      borderWidth: "2px",
-                      borderStyle: "solid",
-                      borderRadius: "3px",
-                    }}
+                    sx={{ width: "100px", flex: "1", marginRight: "5px", borderRadius: "16px", boxShadow: `0px 0px 5px #AB7595` }} /* Apply shadow with border color */
                   />
                   {index === diseases.length - 1 && (
-                    <IconButton
-                      onClick={handleAddDisease}
-                      disabled={diseases.length === 5}
-                      sx={{ color: "#13296A" }}
-                    >
+                    <IconButton onClick={handleAddDisease} disabled={diseases.length === 5} sx={{ color: "blue" }}>
                       <Add />
                     </IconButton>
                   )}
                   {disease.treatments.map((treatment, treatmentIndex) => (
-                    <Box
-                      key={treatmentIndex}
-                      sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        marginLeft: "10px",
-                      }}
-                    >
-                      <IconButton
-                        onClick={() =>
-                          handleRemoveTreatment(index, treatmentIndex)
-                        }
-                        sx={{ color: "#D01717" }}
-                      >
-                        <Remove />
-                      </IconButton>
-                      <TextField
-                        placeholder={`Treatment ${treatmentIndex + 1}`}
-                        value={treatment}
-                        onChange={(e) =>
-                          handleTreatmentChange(index, treatmentIndex, e)
-                        }
-                        sx={{
-                          width: "calc(45% - 30px)",
-                          marginRight: "5px",
-                          borderColor: isSick ? "#AB7595" : undefined,
-                          borderWidth: "2px",
-                          borderStyle: "solid",
-                          borderRadius: "3px",
-                        }}
-                      />
-                      <IconButton
-                        onClick={() => handleAddTreatment(index)}
-                        disabled={disease.treatments.length === 3}
-                        sx={{ color: "#13296A" }}
-                      >
-                        <Add />
-                      </IconButton>
-                    </Box>
-                  ))}
+  <Box key={treatmentIndex} sx={{ display: "flex", alignItems: "center", marginLeft: "10px" }}>
+    <IconButton onClick={() => handleRemoveTreatment(index, treatmentIndex)} sx={{ color: "red" }}>
+      <Remove />
+    </IconButton>
+    <TextField
+      placeholder={`Treatment ${treatmentIndex + 1}`}
+      value={treatment}
+      onChange={(e) => handleTreatmentChange(index, treatmentIndex, e)}
+      sx={{ width: "100px", flex: "1", marginRight: "5px", borderRadius: "16px", boxShadow: `0px 0px 5px #AB7595` }}
+    />
+    {treatmentIndex === disease.treatments.length - 1 && (
+      <IconButton onClick={() => handleAddTreatment(index)} disabled={disease.treatments.length === 3} sx={{ color: "blue" }}>
+        <Add />
+      </IconButton>
+    )}
+  </Box>
+))}
+
                 </Box>
               ))}
             </Box>
           )}
-          <TextField
-            placeholder="Blood Type"
-            value={bloodType}
-            onChange={handleBloodTypeChange}
-            InputProps={{
-              endAdornment: (
-                <IconButton disabled>
-                  <BloodtypeIcon sx={{ color: "red" }} />
-                </IconButton>
-              ),
-            }}
-            sx={{
-              width: "100%",
-              marginTop: "10px",
-              marginBottom: "10px",
-              borderColor: isSick ? "#AB7595" : undefined,
-              borderWidth: "2px",
-              borderStyle: "solid",
-              borderRadius: "3px",
-            }}
-          />
+          <Box sx={{ width: "368px", margin: "auto" }}>
+            <TextField
+  placeholder="Blood Type"
+  value={bloodType}
+  onChange={handleBloodTypeChange}
+  InputProps={{
+    endAdornment: (
+      <IconButton disabled>
+        <BloodtypeIcon sx={{ color: "red" }} />
+      </IconButton>
+    ),
+  }}
+  sx={{
+    marginTop:"10px",
+    marginBottom: "10px",
+    borderRadius: "16px",
+    boxShadow: `0px 0px 5px #AB7595`,
+    width: "100%",
+    height: "53px", 
+  }}
+/>
 
-          <label
-            htmlFor="file-upload"
-            style={{
-              height: "40px",
-              width: "100%",
-              marginBottom: "10px",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-              borderColor: isSick ? "#AB7595" : undefined,
-              borderWidth: "2px",
-              borderStyle: "solid",
-              borderRadius: "3px",
-            }}
-          >
-            <span style={{ marginLeft: "10px", color: "rgba(0,0,0,0.4)" }}>
-              Appload File
-            </span>
-            <FileDownloadIcon sx={{ color: "#AB7595", marginRight: "10px" }} />
-            <input
-              id="file-upload"
-              type="file"
-              onChange={handleFileChange}
-              style={{ display: "none" }}
+            <label htmlFor="file-upload" style={{ height: "53px", width: "100%", marginBottom: "10px", display: "flex", alignItems: "center", justifyContent: "space-between", borderRadius: "20px", boxShadow: `0px 0px 5px #AB7595` }} /* Apply shadow with border color */>
+              <span style={{ marginLeft: "10px", color: "rgba(0,0,0,0.4)" }}>Upload File</span>
+              <FileDownloadIcon sx={{ color: "#AB7595", marginRight: "10px" }} />
+              <input
+                id="file-upload"
+                type="file"
+                onChange={handleFileChange}
+                style={{ display: "none" }}
+              />
+            </label>
+
+
+            <TextField
+              placeholder="Medical Opinion"
+              value={medicalOpinion}
+              onChange={handleMedicalOpinionChange}
+              multiline
+              rows={2.5} sx={{ width: "100%", marginBottom: "10px", borderRadius: "16px", boxShadow: `0px 0px 5px #AB7595` }} /* Apply shadow with border color */
             />
-          </label>
-
-          <TextField
-            placeholder="Medical Opinion"
-            value={medicalOpinion}
-            onChange={handleMedicalOpinionChange}
-            multiline
-            rows={2.5}
-            sx={{
-              width: "100%",
-              borderColor: isSick ? "#AB7595" : undefined,
-              borderWidth: "2px",
-              borderStyle: "solid",
-              borderRadius: "3px",
-            }}
-          />
+          </Box>
           <Box sx={{ display: "flex", justifyContent: "center", mt: 2 }}>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleAccept}
-              sx={{
-                marginRight: "10px",
-                width: "160px",
-                borderRadius: "25px",
-                backgroundColor: "#AB7595",
-              }}
-            >
+            <Button variant="contained" color="primary" onClick={handleAccept} sx={{ marginRight: "10px", width: "160px", borderRadius: "25px", backgroundColor: "#AB7595" }}>
               Accept
             </Button>
-            <Button
-              variant="contained"
-              color="error"
-              onClick={handleRefuse}
-              sx={{
-                width: "160px",
-                borderRadius: "25px",
-                backgroundColor: "#E7D9CA",
-                color: "#000000",
-              }}
-            >
+            <Button variant="contained" color="error" onClick={handleRefuse} sx={{ width: "160px", borderRadius: "25px", backgroundColor: "#E7D9CA", color: "#000000" }}>
               Refuse
             </Button>
           </Box>
@@ -362,3 +222,4 @@ function PatientHealthReview({ onClose, user }) {
 }
 
 export default PatientHealthReview;
+
